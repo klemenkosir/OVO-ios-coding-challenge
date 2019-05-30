ff3bcac0ad691ace032f08a3a6c4a1e04be58eff
Klemen Kosir <klemen.kosir@icloud.com>
2019-05-30 14:43:05 +1000

    - Added basic search functionality

--- OVOIOSCodingChallenge/Controllers/JobListViewController.swift
+++ OVOIOSCodingChallenge/Controllers/JobListViewController.swift
@@ -12,6 +12,7 @@ class JobListViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var infoLabel: UILabel!
+    @IBOutlet private weak var navBarContentView: UIView!
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -19,16 +20,41 @@ class JobListViewController: UIViewController {
     
     private var jobResponse: JobsResponse?
     
+    private var jobs: [Job]? {
+        return filteredJobs ?? jobResponse?.jobs
+    }
+    
+    private var filteredJobs: [Job]? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     private var isLoadingNextPage = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setPullToRefresh()
+        setupSearch()
         
         tableView.rowHeight = UITableView.automaticDimension
 
         loadPage()
+    }
+    
+    private func setupSearch() {
+        let searchVC = SearchViewController.viewController()
+        searchVC.delegate = self
+        searchVC.view.translatesAutoresizingMaskIntoConstraints = false
+        searchVC.willMove(toParent: self)
+        navBarContentView.addSubview(searchVC.view)
+        self.addChild(searchVC)
+        searchVC.didMove(toParent: self)
+        
+        navBarContentView.topAnchor.constraint(equalTo: searchVC.view.topAnchor, constant: 0).isActive = true
+        navBarContentView.bottomAnchor.constraint(equalTo: searchVC.view.bottomAnchor, constant: 0).isActive = true
+        navBarContentView.trailingAnchor.constraint(equalTo: searchVC.view.trailingAnchor, constant: 0).isActive = true
     }
     
     private func setPullToRefresh() {
@@ -79,23 +105,44 @@ class JobListViewController: UIViewController {
 extension JobListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return jobResponse?.jobs.count ?? 0
+        return jobs?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.row+1 == jobResponse!.jobs.count {
+        if indexPath.row+1 == jobs!.count {
             loadNextPage()
         }
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "jobCell", for: indexPath) as! JobCell
-        let job = jobResponse!.jobs[indexPath.row]
+        let job = jobs![indexPath.row]
         cell.set(job: job)
         return cell
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 180
+    }
+    
+}
+
+extension JobListViewController: SearchDelegate {
+    
+    func searchEnded() {
+        filteredJobs = nil
+    }
+    
+    func searchViewDidChange(_ searchString: String?) {
+        guard let lcSearchString = searchString?.lowercased(), !lcSearchString.isEmpty else {
+            filteredJobs = nil
+            return
+        }
+        // very basic implementation of search which just checks if title, advertiser or location contain a given string
+        filteredJobs = jobResponse?.jobs.filter { job -> Bool in
+            return job.title.lowercased().contains(lcSearchString) ||
+                job.advertiser.lowercased().contains(lcSearchString) ||
+                job.location.lowercased().contains(lcSearchString)
+        }
     }
     
 }
---
+++ OVOIOSCodingChallenge/Controllers/SearchViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SearchViewController.swift
+//  OVOIOSCodingChallenge
+//
+//  Created by Klemen Košir on 30/05/2019.
+//  Copyright © 2019 Our Very Own Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchDelegate: class {
+    func searchEnded()
+    func searchViewDidChange(_ searchString: String?)
+}
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet private weak var widthConstraint: NSLayoutConstraint!
+    private var visibleWidthConstraint: NSLayoutConstraint!
+    
+    weak var delegate: SearchDelegate?
+    
+    static func viewController() -> SearchViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "searchViewController") as! SearchViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        visibleWidthConstraint = self.view.widthAnchor.constraint(equalToConstant: 60.0)
+        visibleWidthConstraint.isActive = true
+        
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+    }
+    
+    @IBAction func searchButtonHandler(_ sender: Any) {
+        textField.text = nil
+        textField.becomeFirstResponder()
+        widthConstraint.constant = self.view.superview!.frame.width
+        UIViewPropertyAnimator(duration: 0.35, dampingRatio: 0.85) {
+            self.visibleWidthConstraint.constant = self.view.superview!.frame.width
+            self.view.superview!.layoutIfNeeded()
+        }.startAnimation()
+    }
+    
+    @IBAction func closeButtonHandler(_ sender: Any) {
+        textField.resignFirstResponder()
+        UIViewPropertyAnimator(duration: 0.3, dampingRatio: 0.85) {
+            self.visibleWidthConstraint.constant = 60.0
+            self.view.superview!.layoutIfNeeded()
+            }.startAnimation()
+        delegate?.searchEnded()
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        delegate?.searchViewDidChange(textField.text)
+    }
+    
+}
